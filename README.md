94df4deb80fff5e70323729804eed8585e0b6407
reworks-desktop <dev.dom.re@gmail.com>
2018-07-25 12:51:55 +0800

    project framework

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # warpdrive
 A space adventure game made in C++.
+
+
+Uses C++, Lua and Allegro.
---
+++ warpdrive/src/main.cpp
@@ -0,0 +1,13 @@
+///
+/// main.cpp
+/// warpdrive
+///
+/// Created by reworks on 25/07/2018.
+/// MIT License.
+/// Refer to LICENSE.txt for more details.
+///
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
